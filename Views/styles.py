645bd0e15ce18bdea35167c2d76b8a9030275017
Vinay Pant <vinaypant24@gmail.com>
2023-07-09 23:41:58 +0530

    Initial Files Setup

---
+++ Views/styles.py
@@ -0,0 +1,14 @@
+import colors
+import fonts
+
+
+
+
+class styles():
+
+    def __init__(self) -> None:
+        pass
+
+
+
+    
---
+++ controller.py
@@ -0,0 +1,21 @@
+import sys
+
+
+
+
+
+
+from Views import main_page
+
+
+
+
+
+
+def calling_main_app():
+    pass
+
+
+
+if __name__ == '__main__':
+    calling_main_app()
