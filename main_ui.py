46306df0ae2d11faba5e073a4b54dd50b3a8e4d3
Vinay Pant <vinaypant24@gmail.com>
2024-05-15 04:06:39 +0530

    skeleton Code

--- main_ui.py
+++ main_ui.py
@@ -23,13 +23,21 @@ class Tab_app():
         self.x_location  = (self.tab_based_app.winfo_screenwidth() //2 ) - (self.width //2)
         self.y_location  = (self.tab_based_app.winfo_screenheight() //2 ) - (self.height //2 )
         self.tab_based_app.geometry(f"{self.width}x{self.height}+{self.x_location}+{self.y_location}")
+        
+        self.sidebar = btk.Frame(master=self.tab_based_app , height=(self.height*1) , width=(self.width*.10) , bootstyle  = "success")
+        self.sidebar.pack_propagate(0)
+        
+        self.dark_light_switch  = btk.Checkbutton(master=self.sidebar,bootstyle="error-square-toggle")
 
-        self.sidebar = btk.Frame()
 
 
 
 
 
+
+        ###--- Placing Controls -----------###
+        self.sidebar.pack(side="left")
+        self.dark_light_switch.pack(side="bottom" , padx=0 )
 
 
 
@@ -38,11 +46,12 @@ class Tab_app():
 
 
     def call_app(self):
+
         self.tab_based_app.mainloop()
 
 
 
 if __name__ == "__main__":
-    hello = Tab_app(400 , 500)
+    hello = Tab_app(800 , 900)
     hello.call_app()
 
