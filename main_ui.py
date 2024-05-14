c5d6a3a7a81e1b70e92534a7119d4f3087eb81c0
Vinay Pant <vinaypant24@gmail.com>
2024-05-15 03:54:48 +0530

    Skeleton Code

--- main_ui.py
+++ main_ui.py
@@ -1,9 +1,48 @@
-'''
-Fresh Start just single page for the main app  : 
-
-sidebar which can be open and closed simple app no analytics and not settings just dark and light theme 
-meter widget with the main app and the task is to be marked closed and open depending on the app  : 
+import ttkbootstrap as btk 
 
 
 
-'''
+
+
+
+class Tab_app():
+
+
+    def __init__(self , width  , height ) -> None:
+        self.height = height
+        self.width  = width
+
+
+
+        self.tab_based_app   = btk.Window() # Themes available darkly cyborg and minty cosmo flatly and litera 
+        
+        self.tab_based_app.title("Log Timer")
+        self.tab_based_app.resizable(0 , 0)
+
+        # Make the app in the center of the screen :
+        self.x_location  = (self.tab_based_app.winfo_screenwidth() //2 ) - (self.width //2)
+        self.y_location  = (self.tab_based_app.winfo_screenheight() //2 ) - (self.height //2 )
+        self.tab_based_app.geometry(f"{self.width}x{self.height}+{self.x_location}+{self.y_location}")
+
+        self.sidebar = btk.Frame()
+
+
+
+
+
+
+
+
+
+
+
+
+    def call_app(self):
+        self.tab_based_app.mainloop()
+
+
+
+if __name__ == "__main__":
+    hello = Tab_app(400 , 500)
+    hello.call_app()
+
