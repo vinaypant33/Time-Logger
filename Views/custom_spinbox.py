1fc8d6c67b63d678c3a94cae90d23880b1157532
Vinay Pant <vinaypant24@gmail.com>
2024-06-14 19:02:10 +0530

    Code Save

--- Views/custom_spinbox.py
+++ Views/custom_spinbox.py
@@ -1,49 +1,93 @@
 import tkinter as tk 
 from tkinter import ttk
 import ttkbootstrap as btk 
-
-
-
+from ttkbootstrap.constants import *
+from tkinter import messagebox
 
 
 class SpinBox():
+
+    # style = btk.Style()
+    # style.configure('Custom.TButton', font=("Helvetica", 12), padding=(10, 5))
+
+    def change_number_adding(self):
+        if self.timer_var >= self.max_time:
+            messagebox.showerror("Log Timer" , "Error - Unable to increse timer - Please check max time settings")
+        elif self.timer_var < self.max_time and 0 <=self.timer_var <= 8:
+            self.timer_var+=1
+            self.timer_text.configure(text = str(f"0{self.timer_var}"))
+        elif self.timer_var < self.max_time and 9 <= self.timer_var <= self.max_time:
+            self.timer_var+=1
+            self.timer_text.configure(text = str(f"{self.timer_var}"))
+
+    def change_number_subtracting(self):
+        if self.timer_var <= 0:
+            messagebox.showerror("Log Timer" , "Error - Unable to decrease timer - Please check max time settings")
+        elif self.timer_var <= self.max_time and 11 <= self.timer_var :
+            self.timer_var-=1
+            self.timer_text.configure(text = str(self.timer_var))
+        elif self.timer_var <= self.max_time and self.timer_var <= 11:
+            self.timer_var-=1
+            self.timer_text.configure(text =str(f"0{self.timer_var}"))
+
+        
 
     def __init__(self , master , height  = 400 , width  = 200) -> None:
         self.master = master
         self.height  = height
         self.width  = width 
 
+        self.timer_var  = 00
+        self.max_time = 30
 
-        self.main_frame  = btk.Frame(self.master , height=100 , width=149 , bootstyle  = "warning")
+
+
+    
+
+
+        self.main_frame  = btk.Frame(self.master , height=55 , width=150 , bootstyle  = "warning")
         self.main_frame.pack_propagate(0)
-
-        self.side_frame  = btk.Frame(self.main_frame , height=100 , width=self.width *.6 , bootstyle  = "info")
+        self.side_frame = btk.Frame(self.main_frame , height=55 , width=120 , bootstyle  = "dark")
         self.side_frame.pack_propagate(0)
-        self.main_text = btk.Label(self.side_frame , text="00" , font=("Arial", 20)) # This frame and the fonts and font size and font type is required 
-        self.add_button = btk.Button(self.main_frame , text="+"  ) 
-        self.subtract_button  = btk.Button(self.main_frame , text="-"  )
 
-        self.no_breaks_text  = btk.Label(self.master , text="No breaks in focus session !!" )
+        self.timer_text = btk.Label(self.side_frame , text="00" , font=("Arial", 18))
 
-        ## setting the focus session button with the play and pause Image : 
+        self.add_button = btk.Button(self.main_frame , text="+" , command=self.change_number_adding )
+        self.subtract_button = btk.Button(self.main_frame , text="- " , command=self.change_number_subtracting)
+
+
+
+        # self.side_frame  = btk.Frame(self.main_frame , height=100 , width=self.width *.6 , bootstyle  = "info")
+        # self.side_frame.pack_propagate(0)
+        # self.main_text = btk.Label(self.side_frame , text="00" , font=("Arial", 20)) # This frame and the fonts and font size and font type is required 
+        # self.add_button = btk.Button(self.main_frame , text="+"  ) 
+        # self.subtract_button  = btk.Button(self.main_frame , text="-"  )
+
+        # self.no_breaks_text  = btk.Label(self.master , text="No breaks in focus session !!" )
+
+        # ## setting the focus session button with the play and pause Image : 
+
+
+
 
         
+        # self.main_frame.pack(padx = 50 , pady = 50)
+        # self.side_frame.pack(side = "left")
+        # self.main_text.pack()
+        # self.main_text.place(x = 19 , y = 35)
+        # self.add_button.pack(side = "top" , pady=10)
+        # self.subtract_button.pack(side = "bottom")
+
+        # self.no_breaks_text.place(x = 25 , y =150)
 
 
 
-        
-        self.main_frame.pack(padx = 50 , pady = 50)
-        self.side_frame.pack(side = "left")
-        self.main_text.pack()
-        self.main_text.place(x = 19 , y = 35)
-        self.add_button.pack(side = "top")
-        self.subtract_button.pack(side = "bottom")
+        self.main_frame.pack(pady = 30)
+        self.side_frame.pack(side=tk.LEFT)
+        self.timer_text.pack(pady=(12,0))
 
-        self.no_breaks_text.place(x = 25 , y =150)
-
-
-
-        self.main_frame.pack()
+        self.add_button.pack(side=tk.TOP)
+        self.subtract_button.pack(side=tk.BOTTOM )
 
 
 
--- Views/main_page.py
+++ Views/main_page.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 import ttkbootstrap as btk
 
 
-
+import custom_spinbox
 
 
 class MainPage():
@@ -53,37 +53,28 @@ class MainPage():
         self.theme_frame.pack_propagate(0)
         self.theme_checkbox = btk.Checkbutton(self.theme_frame, text="Change Theme" , bootstyle="dark-square-toggle")
 
+        
 
 
+        text_icon  =  '\u25B6'
+        main_text  = "Start Focus Session"
+        self.play_pause_button  = btk.Button(self.timer_frame , text=f"{text_icon}  { main_text}")
+        self.focus_label  = btk.Label(self.timer_frame , text="Select time ( minutes ) for which the focus session is to be done")
 
         ### Main Controls for Each Frame : 
         # self.timerbox  = btk.Spinbox(self.timer_frame , bootstyle  = "dark" , from_=1, to=100 ) Not gonna use this making a custom control for this 
 
-        
-
-
         # configuring Controls  : 
         self.main_canvas.configure(yscrollcommand=self.scrollbar.set)
-
-        
         
         # Binding Controls  : 
         self.main_canvas.bind('<Configure>' , lambda e: self.main_canvas.configure(scrollregion=self.main_canvas.bbox("all")))
 
-        
         self.main_canvas.create_window((0,0) , window=self.controls_frame ,anchor="nw")
 
-
-
-
-
-
-
-
-
-
         ### Packing the controls  :  Line by Line
-
+        self.focus_label.pack(pady=15)
+        custom_spinbox.SpinBox(self.timer_frame)
         self.bottom_frame.pack(side=tk.BOTTOM)
         self.main_canvas.pack(side=tk.LEFT , fill=tk.BOTH , expand=1)
         self.scrollbar.pack(side=tk.RIGHT , fill=tk.Y)
@@ -97,14 +88,10 @@ class MainPage():
 
         self.theme_checkbox.pack(side="left" ,anchor="center" , pady=4 , padx=15)
 
-    
-
-
+        self.play_pause_button.pack()
         self.main_app.mainloop()
 
     
-
-
 
 
 if __name__ == '__main__':
